ab0464e455465ba0f956a382e97cd90d8288583c
Maria Wyganowska <115419159+marywyg@users.noreply.github.com>
2024-10-27 22:02:24 +0100

    Delete GameOfLife/target directory

--- GameOfLife/target/reports/apidocs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"pl.polsl.lab1.maria.wyganowska.gameoflife"},{"l":"pl.polsl.lab1.maria.wyganowska.gameoflife.controller"},{"l":"pl.polsl.lab1.maria.wyganowska.gameoflife.model"},{"l":"pl.polsl.lab1.maria.wyganowska.gameoflife.view"}];updateSearchResults();
